b73140a463d572254a78d03549678aae5019662d
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2023-09-08 19:16:21 +0900

    [Gold II] Title: 꼬인 전깃줄, Time: 16 ms, Memory: 13740 KB -BaekjoonHub

---
+++ 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.cc
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define INF 2e9
+
+int n, a[1000010], c[1000010], d[1000010];
+vector<int> v;
+
+int main()
+{
+	scanf("%d", &n);
+	
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d", &a[i]);
+	}
+	
+	d[0] = a[0];
+	c[0] = 1;
+	int l = 1;
+	
+	for(int i=0;i<n;i++)
+	{
+		if(a[i] > d[l-1])
+		{
+			d[l++] = a[i];
+			c[i] = l;
+		}
+		int tmp = lower_bound(d, d+l, a[i]) - d;
+		d[tmp] = a[i];
+		c[i] = tmp+1;
+	}
+	
+	printf("%d", n-l);
+	
+}
