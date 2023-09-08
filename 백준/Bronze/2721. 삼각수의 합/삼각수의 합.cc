cc439a48958ab2d5270022022407f8a66f82ab98
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2023-09-08 19:58:49 +0900

    [Bronze III] Title: 삼각수의 합, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2721. 삼각수의 합/삼각수의 합.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		cout<<(long long)(n+3)*(n+2)*(n+1)*(n+0)/8<<"\n";
+	}
+	
+}
