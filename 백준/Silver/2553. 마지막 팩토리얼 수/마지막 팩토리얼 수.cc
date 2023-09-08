e7dd0a8857d41c17136344a73d1a2b8dd830e8e0
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2023-09-08 19:53:18 +0900

    [Silver II] Title: 마지막 팩토리얼 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2553. 마지막 팩토리얼 수/마지막 팩토리얼 수.cc
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	long long ans=1;
+	for(int i=2;i<=n;i++)
+	{
+		ans*=i;
+		while(ans%10==0) ans/=10;
+		ans%=1000000000000;
+	}
+	printf("%lld", ans%10);
+}
