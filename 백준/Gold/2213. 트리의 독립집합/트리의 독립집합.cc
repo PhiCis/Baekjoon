5d35c0acb3c2d4886f8d87aefa16e484f7558e30
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2023-09-08 19:44:21 +0900

    [Gold I] Title: 트리의 독립집합, Time: 8 ms, Memory: 2716 KB -BaekjoonHub

---
+++ 백준/Gold/2213. 트리의 독립집합/트리의 독립집합.cc
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, a[10010];
+vector<int> G[10010];
+int par[10010];
+int cache[10010][2];
+
+int ans[10010];
+
+int f(int k, int checked)
+{
+	if(cache[k][checked]) return cache[k][checked];
+	
+	int ret=0;
+	if(checked)
+	{
+		ret+=a[k];
+		for(auto child:G[k])
+		{
+			if(child==par[k]) continue;
+			par[child]=k;
+			ret+=f(child, 0);
+		}
+		return cache[k][checked]=ret;
+	}
+	else
+	{
+		for(auto child:G[k])
+		{
+			if(child==par[k]) continue;
+			par[child]=k;
+			ret+=max(f(child, 0), f(child, 1));
+		}
+		return cache[k][checked]=ret;
+	}
+}
+
+int main()
+{
+	scanf("%d", &n);
+	for(int i=1;i<=n;i++)
+	{
+		scanf("%d", &a[i]);
+	}
+	for(int i=1;i<n;i++)
+	{
+		int u, v;
+		scanf("%d%d", &u, &v);
+		G[u].push_back(v);
+		G[v].push_back(u);
+	}
+	printf("%d\n", max(f(1, 1), f(1, 0)));
+	for(int i=1;i<=n;i++)
+	{
+		if(ans[par[i]]) continue;
+		if(cache[i][0]<cache[i][1]) ans[i]=1;
+	}
+	for(int i=1;i<=n;i++)
+	{
+		if(ans[i]) printf("%d ", i);
+	}
+}
