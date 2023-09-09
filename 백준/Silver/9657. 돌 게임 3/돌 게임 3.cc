dfbac38e1ab433b6f7b46764bb6e8058996d5bf6
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2023-09-09 14:46:16 +0900

    [Silver III] Title: 돌 게임 3, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.cc
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int n, i, a[1010];
+int main()
+{
+	a[1]=a[3]=a[4]=1;
+	for(i=5;i<=1000;i++)
+	{
+		if(a[i-1]==0||a[i-3]==0||a[i-4]==0)
+		{
+			a[i]=1;
+		}
+		else a[i]=0;
+	}
+	scanf("%d", &n);
+	printf(a[n]==0?"CY":"SK");
+}
