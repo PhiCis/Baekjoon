73b56e073ee16ef74b6871db5e776fdfbfab28de
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2023-09-09 14:25:59 +0900

    [Bronze IV] Title: I Speak TXTMSG, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/6841. I Speak TXTMSG/I Speak TXTMSG.cc
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	while(1)
+	{
+		string s;
+		cin>>s;
+		if(s=="CU") cout<<"see you\n";
+		else if(s==":-)") cout<<"I’m happy\n";
+		else if(s==":-(") cout<<"I’m unhappy\n";
+		else if(s==";-)") cout<<"wink\n";
+		else if(s==":-P") cout<<"stick out my tongue\n";
+		else if(s=="(~.~)") cout<<"sleepy\n";
+		else if(s=="TA") cout<<"totally awesome\n";
+		else if(s=="CCC") cout<<"Canadian Computing Competition\n";
+		else if(s=="CUZ") cout<<"because\n";
+		else if(s=="TY") cout<<"thank-you\n";
+		else if(s=="YW") cout<<"you’re welcome\n";
+		else if(s=="TTYL"){
+			cout<<"talk to you later\n";
+			break;
+		}
+		else cout<<s<<"\n";
+	}
+}
