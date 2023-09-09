eaf64e8c2ac123fd09dabda47bffed68d24310ab
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2023-09-09 13:46:32 +0900

    [Bronze III] Title: 세 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2985. 세 수/세 수.cc
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	scanf("%d%d%d", &a, &b, &c);
+	if(a+b==c) printf("%d+%d=%d", a, b, c);
+	else if(a-b==c) printf("%d-%d=%d", a, b, c);
+	else if(a*b==c) printf("%d*%d=%d", a, b, c);
+	else if(a%b==0&&a/b==c) printf("%d/%d=%d", a, b, c);
+	else if(a==b+c) printf("%d=%d+%d", a, b, c);
+	else if(a==b-c) printf("%d=%d-%d", a, b, c);
+	else if(a==b*c) printf("%d=%d*%d", a, b, c);
+	else if(a==b/c&&b%c==0) printf("%d=%d/%d", a, b, c);
+}
