a1596a7b07bcd0776b5ce59e94d8818493e94d83
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2023-09-09 14:29:12 +0900

    [Bronze II] Title: 팩토리얼, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7489. 팩토리얼/팩토리얼.cc
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int t;
+	scanf("%d", &t);
+	while(t--)
+	{
+		int n;
+		scanf("%d", &n);
+		long long ans=1;
+		for(int i=2;i<=n;i++)
+		{
+			ans*=i;
+			while(ans%10==0) ans/=10;
+			ans%=1000000000000;
+		}
+		printf("%lld\n", ans%10);
+	}
+	
+}
