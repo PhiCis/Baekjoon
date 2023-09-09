5f3be37226d6392387aaa7c9693200a8a4013681
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2023-09-09 13:59:24 +0900

    [Bronze IV] Title: Gnome Sequencing, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4589. Gnome Sequencing/Gnome Sequencing.cc
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	printf("Gnomes:\n");
+	while(n--)
+	{
+		int a, b, c;
+		scanf("%d%d%d", &a, &b, &c);
+		if(a<=b && b<=c) printf("Ordered\n");
+		else if(a>=b && b>=c) printf("Ordered\n");
+		else printf("Unordered\n");
+	}
+}
