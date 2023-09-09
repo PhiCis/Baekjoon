7860dc16074b4810acd6888137eb664f05c4b845
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2023-09-09 14:08:29 +0900

    [Bronze V] Title: Pyramids, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5341. Pyramids/Pyramids.cc
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    while(1)
+    {
+        int n;
+        cin>>n;
+        if(!n) return 0;
+        cout<<n*(n+1)/2<<"\n";
+    }
+}
