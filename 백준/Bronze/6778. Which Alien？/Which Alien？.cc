bf65a09c26b527f484b696c4d3a15afd5a21fde6
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2023-09-09 14:24:48 +0900

    [Bronze IV] Title: Which Alien?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6778. Which Alien？/Which Alien？.cc
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n, m;
+	scanf("%d%d", &n, &m);
+	if(n>=3&&m<=4) printf("TroyMartian\n");
+	if(n<=6&&m>=2) printf("VladSaturnian\n");
+	if(n<=2&&m<=3) printf("GraemeMercurian\n");
+}
