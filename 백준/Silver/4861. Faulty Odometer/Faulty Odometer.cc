8c5ce7547c5e0897fbf97f3d50cef1e3043e3dd4
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2023-09-09 14:02:42 +0900

    [Silver III] Title: Faulty Odometer, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/4861. Faulty Odometer/Faulty Odometer.cc
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cin.tie(0);
+	while(1)
+	{
+		int n, tmp=1, ans=0;
+		cin>>n;
+		if(n==0) return 0;
+		cout<<n<<": ";
+		while(n)
+		{
+			if(n%10>=5) ans+=tmp*(n%10-1);
+			else ans+=tmp*(n%10);
+			n/=10;
+			tmp*=9;
+		}
+		cout<<ans<<"\n";
+	}
+}
