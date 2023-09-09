29e23fb8ae786a9388b002c78374e12e43a51be2
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2023-09-09 14:41:10 +0900

    [Bronze III] Title: Practice: Roll Call, Time: 44 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9306. Practice： Roll Call/Practice： Roll Call.cc
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++)
+	{
+		string a, b;
+		cin>>a>>b;
+		cout<<"Case "<<i<<": "<<b<<", "<<a<<"\n";
+	}
+}
