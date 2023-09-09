4a614dd45be6663d1ea6615177fac13b4371786c
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2023-09-09 14:08:50 +0900

    [Silver V] Title: LCM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/5347. LCM/LCM.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int t;
+	scanf("%d", &t);
+	while(t--)
+	{
+		int a, b;
+		scanf("%d%d", &a, &b);
+		printf("%lld\n", (long long)a/__gcd(a, b)*b);
+	}
+	
+}
