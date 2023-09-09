abe8c2e2ce0edc21485b4e1349c893b85525c726
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2023-09-09 14:44:09 +0900

    [Bronze I] Title: 약수들의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.cc
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int a;
+
+int main()
+{
+	while(1)
+	{
+		scanf("%d", &a);
+		if(a==-1)
+		{
+			break;
+		}
+		if(a==6)
+		{
+			printf("6 = 1 + 2 + 3\n");
+		}
+		else if(a==28)
+		{
+			printf("28 = 1 + 2 + 4 + 7 + 14\n");
+		}
+		else if(a==496)
+		{
+			printf("496 = 1 + 2 + 4 + 8 + 16 + 31 + 62 + 124 + 248\n");
+		}
+		else if(a==8128)
+		{
+			printf("8128 = 1 + 2 + 4 + 8 + 16 + 32 + 64 + 127 + 254 + 508 + 1016 + 2032 + 4064\n");
+		}
+		else printf("%d is NOT perfect.\n", a);
+	}
+}
