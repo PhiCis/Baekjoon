92bcad01750d2eb5790642567e3809eea3619455
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2023-09-09 16:23:32 +0900

    [Bronze I] Title: 근무 지옥에 빠진 푸앙이 (Small), Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/25593. 근무 지옥에 빠진 푸앙이 （Small）/근무 지옥에 빠진 푸앙이 （Small）.cc
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n;
+map<string, int> m;
+int d[5]={0, 4, 6, 4, 10};
+vector<int> v;
+
+int main()
+{
+	cin>>n;
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=4;j++)
+		{
+			for(int k=1;k<=7;k++)
+			{
+				string s;
+				cin>>s;
+				if(s=="-") continue;
+				m[s]+=d[j];
+			}
+		}
+	}
+	for(auto it=m.begin();it!=m.end();it++)
+	{
+		v.push_back(it->second);
+	}
+	sort(v.begin(), v.end());
+	if(v.size()==0 || v[v.size()-1]-v[0]<=12) cout<<"Yes";
+	else cout<<"No";
+}
