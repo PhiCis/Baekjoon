c48c311cd1240712bdd2fe41f1fcedf10d7ac76b
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2023-09-12 15:34:19 +0900

    [Bronze III] Title: Automated Checking Machine, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10395. Automated Checking Machine/Automated Checking Machine.cc
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a, b, c, d, e, f, g, h, i, j;
+	scanf("%d%d%d%d%d%d%d%d%d%d", &a, &b, &c, &d, &e, &f, &g, &h, &i, &j);
+	if(a+f==1&&b+g==1&&c+h==1&&d+i==1&&e+j==1) printf("Y");
+	else printf("N");
+}
