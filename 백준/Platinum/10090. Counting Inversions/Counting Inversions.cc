6f3ff8805d76e4e61de65ddb04cb976c48405421
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2023-09-12 15:29:18 +0900

    [Platinum V] Title: Counting Inversions, Time: 256 ms, Memory: 8928 KB -BaekjoonHub

---
+++ 백준/Platinum/10090. Counting Inversions/Counting Inversions.cc
@@ -0,0 +1,67 @@
+#include<stdio.h>
+
+int n;
+int a[1000010], b[1000010];
+
+long long ans;
+void merge(int s, int m, int e)
+{
+	int h=s;
+	int i=s, j=m+1;
+	while(i<=m&&j<=e)
+	{
+		if(a[i]<=a[j])
+		{
+			b[h]=a[i];
+			h++;
+			i++;
+		}
+		else
+		{
+			b[h]=a[j];
+			ans+=m-i+1;
+			//printf("%d %d %d\n", a[i], a[j], m-i+1);
+			h++;
+			j++;
+		}
+	}
+	if(i>m)
+	{
+		for(;j<=e;j++)
+		{
+			b[h++]=a[j];
+		}
+	}
+	else
+	{
+		for(;i<=m;i++)
+		{
+			b[h++]=a[i];
+		}
+	}
+	
+	for(h=s;h<=e;h++)
+	{
+		a[h]=b[h];
+	}
+}
+void mergesort(int s, int e)
+{
+	if(s<e)
+	{
+		int m=(s+e)/2;
+		mergesort(s, m);
+		mergesort(m+1, e);
+		merge(s, m, e);
+	}
+}
+int main()
+{
+	scanf("%d", &n);
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d", &a[i]);
+	}
+	mergesort(0,n-1);
+	printf("%lld", ans);
+}
