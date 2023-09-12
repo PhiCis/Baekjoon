a8fc68c4f6b4a19efcad0bdd2c66d9efcdd6e1f3
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2023-09-12 15:36:52 +0900

    [Bronze III] Title: 페르시아의 왕들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10599. 페르시아의 왕들/페르시아의 왕들.cc
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a, b, c, d;
+	while(1)
+	{
+		scanf("%d%d%d%d", &a, &b, &c, &d);
+		if(a==0&&b==0&&c==0&&d==0) return 0;
+		printf("%d %d\n", c-b, d-a);
+	}
+	
+}
