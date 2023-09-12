08f68de99c3651d467d0aa0cb33653b2dae5f5d2
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2023-09-12 16:02:43 +0900

    [Bronze III] Title: Hedgemony (Small), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/12351. Hedgemony （Small）/Hedgemony （Small）.cc
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int t, n;
+double a[11];
+int main()
+{
+	scanf("%d", &t);
+	for(int i=1;i<=t;i++)
+	{
+		scanf("%d", &n);
+		for(int j=1;j<=n;j++)
+		{
+			scanf("%lf", &a[j]);
+		}
+		for(int j=2;j<n;j++)
+		{
+			a[j]=min(a[j], 0.5*(a[j-1]+a[j+1]));
+		}
+		printf("Case #%d: %lf\n", i, a[n-1]);
+	}
+}
