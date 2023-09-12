ce333f1a36f0d2bcc0db6c93a04b69c1336e14ed
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2023-09-12 16:04:25 +0900

    [Bronze III] Title: 고급 여관, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/12756. 고급 여관/고급 여관.cc
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a, b, c, d;
+	scanf("%d%d%d%d", &a, &b, &c, &d);
+	while(b>0&&d>0)
+	{
+		b-=c, d-=a;
+	}
+	if(b<=0&&d<=0)
+	{
+		printf("DRAW");
+	}
+	else if(b<=0)
+	{
+		printf("PLAYER B");
+	}
+	else
+	{
+		printf("PLAYER A");
+	}
+}
