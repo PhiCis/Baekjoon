ae4d476267bdaa27385aa2a674ed5f951f5a0948
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2023-09-12 15:49:58 +0900

    [Silver II] Title: 최대 힙, Time: 24 ms, Memory: 2008 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<queue>
+using namespace std;
+priority_queue<int,vector<int>,less<int> > pq;
+int n, i, a;
+int main()
+{
+	scanf("%d", &n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d", &a);
+		if(a==0)
+		{
+			if(pq.size()==0) printf("0\n");
+			else
+			{
+				printf("%d\n", pq.top());
+				pq.pop();
+			}
+		}
+		else
+		{
+			pq.push(a);
+		}
+	}
+}
