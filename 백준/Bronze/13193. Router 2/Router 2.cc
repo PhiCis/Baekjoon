1f01edbf4e8c7cbeef99f3a92834adf2b557d66e
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2023-09-12 16:09:17 +0900

    [Bronze I] Title: Router 2, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13193. Router 2/Router 2.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n=223;
+	printf("%d %d\n", 2*n, n*n);
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=n;j++)
+		{
+			printf("%d %d\n", i, n+j);
+		}
+	}
+}
