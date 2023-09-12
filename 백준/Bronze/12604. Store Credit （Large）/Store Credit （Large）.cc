9b94e9a530ae6449d44b7489b6a9a4a6013f220c
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2023-09-12 16:03:50 +0900

    [Bronze II] Title: Store Credit (Large), Time: 8 ms, Memory: 2040 KB -BaekjoonHub

---
+++ 백준/Bronze/12604. Store Credit （Large）/Store Credit （Large）.cc
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int t, n, m, a[5010];
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin>>t;
+	for(int i=1;i<=t;i++)
+	{
+		cin>>n>>m;
+		for(int j=1;j<=m;j++)
+		{
+			cin>>a[j];
+		}
+		for(int j=1;j<=m;j++)
+		{
+			for(int k=j+1;k<=m;k++)
+			{
+				if(a[j]+a[k]==n)
+				{
+					cout<<"Case #"<<i<<": "<<j<<" "<<k<<"\n";
+					goto skip;
+				}
+			}
+		}
+		skip:;
+	}
+}
