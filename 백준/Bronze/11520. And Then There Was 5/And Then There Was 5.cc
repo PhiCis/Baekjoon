69c4d046cc6f13531f1fd4185b8f1af6fa6f6ab9
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2023-09-12 15:53:45 +0900

    [Bronze III] Title: And Then There Was 5, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11520. And Then There Was 5/And Then There Was 5.cc
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int t, a, b;
+
+int main()
+{
+	scanf("%d", &t);
+	for(int i=1;i<=t;i++)
+	{
+		scanf("%d%d", &a, &b);
+		printf("%d %d\n", b, 5);
+	}
+}
