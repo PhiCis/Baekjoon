efb61517e79f33838a11dc950908e2efba6dc8a7
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2023-09-18 08:54:21 +0900

    [Gold V] Title: 감소하는 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.cc
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<long long> v;
+
+void f(long long n, int m)
+{
+    v.push_back(n);
+    for(int i=m-1;i>=0;i--) f(n*10+i, i);
+}
+
+int main()
+{
+    for(int i=9;i>=0;i--) f(i, i);
+    sort(v.begin(), v.end());
+    int n;
+    cin>>n;
+    if(v.size()>=n+1) cout<<v[n];
+    else cout<<-1;
+}
