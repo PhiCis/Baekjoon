246ffa628c0a861c318cbe18f37867ab29023dd0
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2023-09-20 16:38:03 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,10 @@
+n=int(input())
+for i in range(n):
+    a=input()
+    if a=="Algorithm": print(204)
+    if a=="DataAnalysis": print(207)
+    if a=="ArtificialIntelligence": print(302)
+    if a=="CyberSecurity": print("B101")
+    if a=="Network": print(303)
+    if a=="Startup": print(501)
+    if a=="TestStrategy": print(105)
