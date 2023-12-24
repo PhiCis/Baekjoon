c53f8c6437d381b7a3883cc9d309fda93a8c2633
Cisū Pai <63911295+PhiCis@users.noreply.github.com>
2023-12-24 21:39:55 +0900

    [Bronze IV] Title: 감마선을 맞은 컴퓨터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.cc
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	char a;
+	for(int i=1;i<=225;i++)
+	{
+		cin>>a;
+		if(a=='w')
+		{
+			cout<<"chunbae";
+			return 0;
+		}
+		if(a=='b')
+		{
+			cout<<"nabi";
+			return 0;
+		}
+		if(a=='g')
+		{
+			cout<<"yeongcheol";
+			return 0;
+		}
+	}
+}
