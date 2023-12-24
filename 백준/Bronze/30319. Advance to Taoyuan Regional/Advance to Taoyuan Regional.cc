00ef86bc2a2062bbbb846f04da4102f42a9e1a3b
Cisū Pai <63911295+PhiCis@users.noreply.github.com>
2023-12-24 22:13:13 +0900

    [Bronze IV] Title: Advance to Taoyuan Regional, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30319. Advance to Taoyuan Regional/Advance to Taoyuan Regional.cc
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int a, b, c;
+
+int main()
+{
+	scanf("%d-%d-%d", &a, &b, &c);
+	if(a*10000+b*100+c<=20230916) cout<<"GOOD";
+	else cout<<"TOO LATE";
+}
