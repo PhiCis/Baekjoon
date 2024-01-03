b6caf538ff90ef98e2b6a917857b5308b5d6a466
Cisū Pai <63911295+PhiCis@users.noreply.github.com>
2024-01-04 01:21:51 +0900

    [Platinum V] Title: 찾기, Time: 64 ms, Memory: 11424 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.cc
+++ 백준/Platinum/1786. 찾기/찾기.cc
@@ -1,53 +1,46 @@
-#include <bits/stdc++.h>
-using namespace std; 
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <vector>
+using namespace std;
 
-vector<int> getPi(string p)
-{ 
-	int m = (int)p.size(), j=0; 
-	vector<int> pi(m, 0); 
-	for(int i = 1; i< m ; i++)
-	{ 
-		while(j > 0 && p[i] != p[j]) j = pi[j-1]; 
-		
-		if(p[i] == p[j]) pi[i] = ++j;
-	} 
-	return pi;
-} 
+string s, p;
+vector<int> pi, ans;
 
-vector<int> kmp(string s, string p)
-{ 
-	vector<int> ans; 
-	auto pi = getPi(p); 
-	int n = (int)s.size(), m = (int)p.size(), j =0; 
-	
-	for(int i = 0 ; i < n ; i++)
-	{ 
-		while(j>0 && s[i] != p[j]) j = pi[j-1]; 
-		
-		if(s[i] == p[j])
-		{
-			if(j==m-1)
-			{ 
-				ans.push_back(i-m+1); 
-				j = pi[j]; 
-			}
-			else
-			{ 
-				j++; 
-			} 
-		} 
-	} 
-	return ans; 
-} 
-
+void kmp(string s, string p, vector<int>& f)
+{
+	int n=s.size(), m=p.size();
+	int j=0;
+	for(int i=1;i<n;i++)
+	{
+		while((j>0&&s[i]!=p[j])||j==m) j=pi[j-1];
+		if(s[i]==p[j]) f[i]=++j;
+	}
+}
 int main()
-{ 
-	string s, p; 
-	getline(cin, s); 
-	getline(cin, p); 
-	auto matched = kmp(s,p); 
-	printf("%d\n", (int)matched.size()); 
-	for(auto i : matched) 
-		printf("%d ", i+1); 
-	return 0; 
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(0);
+	getline(cin, s);
+	getline(cin, p);
+    s="$"+s;
+    pi.resize(p.size());
+    ans.resize(s.size());
+    
+    kmp(p, p, pi);
+    kmp(s, p, ans);
+    
+    int ANS=0;
+    for(int i=1;i<s.size();i++)
+    {
+    	if(ans[i]==p.size()) ANS++;
+	}
+	cout<<ANS<<"\n";
+	for(int i=1;i<s.size();i++)
+	{
+		if(ans[i]==p.size())
+		{
+			cout<<i-p.size()+1<<" ";
+		}
+	}
 }
