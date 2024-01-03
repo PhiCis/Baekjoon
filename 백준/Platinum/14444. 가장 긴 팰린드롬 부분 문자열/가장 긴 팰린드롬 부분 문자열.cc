dbadd90e482ebad6dc98f0865f763511b401d478
Cisū Pai <63911295+PhiCis@users.noreply.github.com>
2024-01-03 17:44:16 +0900

    [Platinum V] Title: 가장 긴 팰린드롬 부분 문자열, Time: 8 ms, Memory: 3296 KB -BaekjoonHub

--- 백준/Platinum/14444. 가장 긴 팰린드롬 부분 문자열/가장 긴 팰린드롬 부분 문자열.cc
+++ 백준/Platinum/14444. 가장 긴 팰린드롬 부분 문자열/가장 긴 팰린드롬 부분 문자열.cc
@@ -1,76 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, r, k;
-int p1[100010], p2[200020];
-int max1, max1i, max2, max2i;
+
+string s, t;
+int a[200200];
+int ans;
+
 int main()
 {
-	string s1, s2;
-	cin>>s1;
-	//odd
-	n=s1.size(),r=-1,k=-1;
-    for(int i=0;i<n;i++) 
+	cin>>t;
+	s="$";
+	for(int i=0;i<t.size();i++)
 	{
+		s+=t[i];
+		s+="$";
+	}
+	int n=s.size();
+	
+	int r=0, p=0;
+	for(int i=0;i<n;i++)
+	{
+		a[i]=0;
 		if(i<=r)
 		{
-			p1[i]=min(r-i, p1[2*k-i]);
-		}
-		while(i-p1[i]-1>=0&&i+p1[i]+1<n&&s1[i-p1[i]-1]==s1[i+p1[i]+1])
-		{
-			p1[i]++;
-		}
-		if(r<i+p1[i])
-		{
-			r=i+p1[i];
-			k=i;
-		}
-        if(max1<p1[i])
-        {
-        	max1=p1[i];
-        	max1i=i;
-		}
-    }
-    //even
-    if(n>=2)
-    {
-    	s2=" ";
-    	for(int i=0;i<n;i++)
-	    {
-	    	s2+=s1[i];
-	    	s2+=" ";
-		}
-		
-		n=s2.size(),r=-1,k=-1;
-	    for(int i=0;i<n;i++) 
-		{
-			if(i<=r)
+			if(a[2*p-i]<r-i)
 			{
-				p2[i]=min(r-i, p2[2*k-i]);
+				a[i]=a[2*p-i];
+				goto skip;
 			}
-			while(i-p2[i]-1>=0&&i+p2[i]+1<n&&s2[i-p2[i]-1]==s2[i+p2[i]+1])
-			{
-				p2[i]++;
-			}
-			if(r<i+p2[i])
-			{
-				r=i+p2[i];
-				k=i;
-			}
-	        if(max2<p2[i]&&s2[i]==' ')
-	        {
-	        	max2=p2[i];
-	        	max2i=i;
-			}
-	    }
+			else a[i]=r-i;
+		}
+		while(i-a[i]-1>=0&&i+a[i]+1<n&&s[i-a[i]-1]==s[i+a[i]+1]) a[i]++;
+		if(r<i+a[i])
+		{
+			p=i;
+			r=p+a[p];
+		}
+		skip:;
 	}
-	//printf("%d %d\n", max1, max2);
-	if(max1>=max2/2)
+	for(int i=0;i<n;i++)
 	{
-        cout<<max1*2+1;
+		ans=max(ans, a[i]);
 	}
-	else
-	{
-		cout<<max2;
-	}
-    
+	cout<<ans;
+	
 }
