09c2a7780d5fbdb70cb8d3e4f0b61722e877d171
Cisū Pai <63911295+PhiCis@users.noreply.github.com>
2024-01-03 16:30:05 +0900

    [Platinum V] Title: 가장 긴 팰린드롬 부분 문자열, Time: 4 ms, Memory: 6616 KB -BaekjoonHub

---
+++ 백준/Platinum/13275. 가장 긴 팰린드롬 부분 문자열/가장 긴 팰린드롬 부분 문자열.cc
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string s, t;
+int a[1000010], n;
+int ans;
+
+void manachers(string s, int n)
+{
+	int r=0, p=0;
+	for(int i=0;i<n;i++)
+	{
+		if(i<=r) a[i]=min(a[2*p-i], r-i);
+		else a[i]=0;
+		while(i-a[i]-1>=0 && i+a[i]+1<n && s[i-a[i]-1]==s[i+a[i]+1]) a[i]++;
+		if(r<i+a[i])
+		{
+			r=i+a[i];
+			p=i;
+		}
+	}
+}
+
+
+int main()
+{
+	cin>>s;
+	t="$";
+	for(int i=0;i<s.size();i++)
+	{
+		t+=s[i];
+		t+="$";
+	}
+//	cout<<t;
+	manachers(t, t.size());
+//	for(int i=0;i<t.size();i++) cout<<a[i];
+	for(int i=0;i<t.size();i++)
+	{
+		ans=max(ans, a[i]);
+	}
+	cout<<ans;
+}
