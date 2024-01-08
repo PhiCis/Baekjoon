ea8732225156193657153fa248fde55158f0aae2
Cisū Pai <63911295+PhiCis@users.noreply.github.com>
2024-01-08 16:57:10 +0900

    [Silver III] Title: Password Problem (Large), Time: 232 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/12395. Password Problem （Large）/Password Problem （Large）.cc
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, m;
+double a[100100];
+
+int main()
+{
+	int t;
+	cin>>t;
+	for(int z=1;z<=t;z++)
+	{
+		
+		cin>>n>>m;
+		double ans=m+2;
+		double p=1;
+		for(int i=1;i<=n;i++)
+		{
+			cin>>a[i];
+			p*=a[i];
+//			cout<< (n-i)+(p*(m-i+1)+(1-p)*(m-i+1+m+1));
+			ans=min(ans, (n-i)+(p*(m-i+1)+(1-p)*(m-i+1+m+1)));
+		}
+		cout.precision(10);
+		cout<<"Case #"<<z<<": ";
+		cout<<ans<<"\n";
+	}
+}
