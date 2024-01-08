7a86692c940122fa4ff02aaf7217154d76aeccff
Cisū Pai <63911295+PhiCis@users.noreply.github.com>
2024-01-08 15:24:23 +0900

    [Silver II] Title: Ocean View (Large), Time: 8 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/12355. Ocean View （Large）/Ocean View （Large）.cc
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int t, n, a[1100], dp[1100];
+int cnt;
+
+int main()
+{
+	scanf("%d", &t);
+	while(t--)
+	{
+		cnt++;
+		scanf("%d", &n);
+		for(int i=1;i<=n;i++)
+		{
+			dp[i]=1;
+			scanf("%d", &a[i]);
+			for(int j=1;j<i;j++)
+			{
+				if(a[i]>a[j]) dp[i]=max(dp[i], dp[j]+1);
+			}
+		}
+		int ans = 0;
+		for(int i=1;i<=n;i++)
+		{
+			ans = max(ans, dp[i]);
+		}
+		printf("Case #%d: %d\n", cnt, n-ans);
+	}
+}
